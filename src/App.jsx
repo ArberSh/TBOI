68fc01d78069def97d27fa33e54f16e79d8bac62
Arber Shaska <arbershaska2@gmail.com>
2026-03-31 23:47:28 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,12 @@ function App() {
 fetch('https://raw.githubusercontent.com/Rchardon/RebirthItemTracker/master/items.json')
   .then(res => res.json())
   .then(data => console.log(data))
-
+  
   
   return (
 
     <>
-
+      <h1> The Binding of Isaac: MiniGames</h1>
     </>
   )
 }
