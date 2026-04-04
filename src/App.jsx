31fda1bde7f6fef3fdb9bdf046bdca3055dae01d
Arber Shaska <arbershaska2@gmail.com>
2026-04-04 23:21:55 +0200

    added background img and other stuffs

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
 import { useState } from 'react'
 
 import './App.css'
+import GuessTheItem from './GuessTheItem'
 
 function App() {
   // const [count, setCount] = useState(0)
-fetch('https://raw.githubusercontent.com/Rchardon/RebirthItemTracker/master/items.json')
-  .then(res => res.json())
-  .then(data => console.log(data))
+
   
   
   return (
-
     <>
-      <h1> The Binding of Isaac: MiniGames</h1>
-      <h2>Guess the name of the item</h2>
-      <img src="" alt="" />
+      <GuessTheItem/>
     </>
   )
 }
---
+++ src/GuessTheItem.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./GuessTheItem.css"
+
+function GuessTheItem() {
+  return (
+    <div className='Container-all'>
+
+        <div>
+            <h1>hi</h1>
+        </div>
+
+    </div>
+  )
+}
+
+export default GuessTheItem
