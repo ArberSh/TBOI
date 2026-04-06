06764610209fa86b7451c345bd6026109e3a2e04
Arber Shaska <arbershaska2@gmail.com>
2026-04-06 23:37:03 +0200

    added some stuffs

--- src/GuessTheItem.jsx
+++ src/GuessTheItem.jsx
@@ -8,7 +8,12 @@ function GuessTheItem() {
         <div className='Container'>
             <h1>Can you guess this item?</h1>
             <div className='Container-image'>
-            <img src="/public/images/items/canvas.png" alt="" />
+            <img src="/public/images/items/canvas.png" alt="" /> 
+            {/* provo t gjesh te github fotot  */}
+            </div>
+            <div>
+              <input type="text" name="" id="" />
+              <button>Submit</button>
             </div>
         </div>
 
