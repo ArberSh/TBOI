cdbf566bff1549f7b0a6b40b6fee5081550b3551
Arber Shaska <arbershaska2@gmail.com>
2026-06-05 00:01:33 +0200

    supabase fixed

--- src/supabase.js
+++ src/supabase.js
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   'https://zczeouminjzkrcxyrgwk.supabase.co',
-  'sb_publishable_8-T8WJJDwaiT1DQ96IiimQ_gplUhnAK'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpjemVvdW1pbmp6a3JjeHlyZ3drIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAxMjUyNzYsImV4cCI6MjA5NTcwMTI3Nn0.Zf1uSUeMHR_9VQaml9p8NIOFr9OuM39gsPs5v4yRSno'
 )
